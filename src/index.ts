0e4b24f466010807e9965169f6e4fac126db4202
Atom <chanidapa.atom@gmail.com>
2026-07-21 00:03:54 +0700

    lab07

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,12 @@ app.use(morgan("dev", { immediate: false }));
 app.use(express.json()); // parses request's payload into 'req.body'
 
 // Endpoints
+app.get("/", (req: Request, res: Response) => {
+  return res.json({
+    message: "API services for Student Data",
+  });
+});
+
 app.get("/api/me", (req: Request, res: Response) => {
   return res.json({
     ok: true,
